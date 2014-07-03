c50ac3b18a45510df6811feef2b513513f3d74ec
Hyan Mandian <hyanmandian@hotmail.com>
2014-07-03 02:23:48 -0300

    Refactor factory

--- app/Classes/Model/Database.php
+++ app/Classes/Model/Database.php
@@ -4,27 +4,32 @@ namespace Classes\Model;
 
 class Database {
 
-    public static function factory($vo) {
-        $class = NULL;
+    private $types = array();
 
-        switch ($vo) {
-            case "Patient":
-                $class = new \Classes\Model\DAO\Patient;
-                break;
-            case "Medic":
-                $class = new \Classes\Model\DAO\Medic;
-                break;
-            case "Consultation":
-                $class = new \Classes\Model\DAO\Consultation;
-                break;
-            default:
-                throw new Exception("Class not exist");
-        }
-
-        if ($class instanceof \Classes\Model\DAO\DAO) {
-            return $class;
-        } else {
-            throw new Exception("Class not exist");
-        }
+    public function __construct() {
+        $this->configureDefaults();
     }
+
+    private function configureDefaults() {
+        $this->setClass("patient", "Patient");
+        $this->setClass("medic", "Medic");
+        $this->setClass("consultation", "Consultation");
+    }
+
+    private function setClass($type, $class) {
+        if (!class_exists($class, TRUE)) {
+            throw new Exception("Invalid type");
+        }
+
+        $this->types[$type] = $class;
+    }
+
+    public static function factory($vo) {
+        if (!isset($this->types[$tipo])) {
+            throw new Exception("Invalid type");
+        }
+
+        return new $this->types[$tipo];
+    }
+
 }
